52cd4a4286379cd9dfd5074254d7b84851f0b17d
Kyle-sigma-specter <kylecansino11@gmail.com>
2024-12-04 18:06:33 +0800

    Add files via upload

---
+++ Website/Index.js
@@ -0,0 +1,85 @@
+let name_user
+
+document.getElementById("mysubmit").onclick = function(){
+  name_user = document.getElementById("mytext").value;
+  document.getElementById("mys12").textContent = `Hello, ${name_user}`
+}
+
+
+
+
+//let usernam2314
+
+//usernam2314 = window.prompt("enter your name bbg")
+
+
+let age2
+age2 = window.prompt("how old are you")
+
+age +=1
+
+console.log(age)
+t
+x
+ 
+console.log(`sigma sigma boy`);
+console.log(`I dunno what to type ngl`);
+//Console log in inspect
+//window.alert(`This is an alert!`);
+//window.alert(`I LOVE CODING`)
+//Shows a notification alert
+
+document.getElementById("MyH1").textContent = `Hello broski`
+document.getElementById("MyPP").textContent = `I love skibidi toilet`
+document.getElementById("MyH2").textContent = `what if i lick you?`
+
+let age = 100
+document.getElementById("dada").textContent = `you are ${age} years old you old beta`
+
+let isStudent = false
+document.getElementById("juj").textContent = `is student: ${isStudent}`
+
+let skibiditoilet = "cringe"
+document.getElementById("s1").textContent = `:(`
+
+document.getElementById("s2").textContent = `If you're cringe get the hell out`;
+
+
+//variable = A container that stores a value.
+  //         Behaves as if it were the value it contains.
+
+    //            1.Declaration   let x;
+      //          2. assignment = 100; 
+      //    Declaration and assignment can be combined
+   //   example: Let x = "100"
+   // console.log(typeof (var)); to get variable type    
+   // boolean variable -   let (variable) = true/false;
+
+      let x;
+      x = (100);
+      console.log(x)
+
+    console.log(ylli);
+
+    let Username = ("Kyle-sigma-specter");
+    let Username2 = ("ylli");
+    console.log(Username2);
+    console.log(Username)
+    
+    
+    
+
+let students = 30 
+
+
+
+//students = students + 1  -Addition
+//students = students * 200 -multiplication
+//students = students / 5   -Division
+//students = students - 10  -Subtraction
+//students = students % 11  -remainder
+students += 30
+
+document.getElementById("math").textContent = (` *busts all over the students* ${students}`)
+
+
